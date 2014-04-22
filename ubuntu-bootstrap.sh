dc1d84533783a6d985fa300cb8d8a058af44e56e
David Reagan <reagand@lanecc.edu>
2014-04-21 22:54:04 -0700

    Made ubuntu-bootstrap.sh echo ubuntu mirror deb lines into sources.list so that apt is faster.

--- ubuntu-bootstrap.sh
+++ ubuntu-bootstrap.sh
@@ -5,3 +5,11 @@ echo "192.168.88.3 vm.centos.lab" >> /etc/hosts
 echo "192.168.88.4 vm.sles.lab" >> /etc/hosts
 echo "192.168.88.5 vm.oracle59.lab" >> /etc/hosts
 cat /vagrant/userpubkey.pub >> /home/vagrant/.ssh/authorized_keys
+echo "deb mirror://mirrors.ubuntu.com/mirrors.txt precise main restricted universe multiverse" > /vagrant/tmp/newapt
+echo "deb mirror://mirrors.ubuntu.com/mirrors.txt precise-updates main restricted universe multiverse" >> /vagrant/tmp/newapt
+echo "deb mirror://mirrors.ubuntu.com/mirrors.txt precise-backports main restricted universe multiverse" >> /vagrant/tmp/newapt
+echo "deb mirror://mirrors.ubuntu.com/mirrors.txt precise-security main restricted universe multiverse" >>/vagrant/tmp/newapt
+cat /etc/apt/sources.list >> /vagrant/tmp/newapt
+cp /vagrant/tmp/newapt /etc/apt/sources.list
+#apt-get update
+#apt-get -y dist-upgrade
