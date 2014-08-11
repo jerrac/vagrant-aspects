fb7147ec8cc72a45b25e66d58eb658d0639b4d69
David Reagan <reagand@lanecc.edu>
2014-08-10 19:48:47 -0700

    Added ubuntutrusty bootstrap stuff.

--- ubuntutrusty-bootstrap.sh
+++ ubuntutrusty-bootstrap.sh
@@ -6,10 +6,10 @@ echo "192.168.88.4 vm.sles.lab" >> /etc/hosts
 echo "192.168.88.5 vm.oracle59.lab" >> /etc/hosts
 echo "192.168.88.6 vm.trustyubuntu.lab" >> /etc/hosts
 cat /vagrant/userpubkey.pub >> /home/vagrant/.ssh/authorized_keys
-echo "deb mirror://mirrors.ubuntu.com/mirrors.txt precise main restricted universe multiverse" > /vagrant/tmp/newapt
-echo "deb mirror://mirrors.ubuntu.com/mirrors.txt precise-updates main restricted universe multiverse" >> /vagrant/tmp/newapt
-echo "deb mirror://mirrors.ubuntu.com/mirrors.txt precise-backports main restricted universe multiverse" >> /vagrant/tmp/newapt
-echo "deb mirror://mirrors.ubuntu.com/mirrors.txt precise-security main restricted universe multiverse" >>/vagrant/tmp/newapt
+echo "deb mirror://mirrors.ubuntu.com/mirrors.txt trusty main restricted universe multiverse" > /vagrant/tmp/newapt
+echo "deb mirror://mirrors.ubuntu.com/mirrors.txt trusty-updates main restricted universe multiverse" >> /vagrant/tmp/newapt
+echo "deb mirror://mirrors.ubuntu.com/mirrors.txt trusty-backports main restricted universe multiverse" >> /vagrant/tmp/newapt
+echo "deb mirror://mirrors.ubuntu.com/mirrors.txt trusty-security main restricted universe multiverse" >>/vagrant/tmp/newapt
 cat /etc/apt/sources.list >> /vagrant/tmp/newapt
 cp /vagrant/tmp/newapt /etc/apt/sources.list
 #trying to install packages on a clean install doesn't work without running this first.
